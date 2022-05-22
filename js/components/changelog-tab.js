ecec0dba79ec733c3f63f82af2817855981bc46f
difficultcomplexity <19alex88kuzich@gmail.com>
2022-05-22 17:27:08 +0300

    v1.0.2

--- js/components/changelog-tab.js
+++ js/components/changelog-tab.js
@@ -1,6 +1,14 @@
 Vue.component("changelog-tab", {
     template: `<div class="changelog-tab">
     <guide-item>
+    <template v-slot:title>v1.0.2</template>
+    <template v-slot:text>
+    Changes to names, 50+ new news tickers! 
+    Switched names from ω2Engine to ω2 Layers. 
+    Loss of savefiles are certain. If you are currently having loss on data, the save was invalid because it had ω2Engine in it.
+    </template>
+    </guide-item>
+    <guide-item>
     <template v-slot:title>v1.0.1</template>
     <template v-slot:text>
     Added 10+ new achievements!
